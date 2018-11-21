905a28e2e4fa362dce5f9f8cf913807c32e5e5a5
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-21 11:52:04 +0800

    finished lambda to practice 1-1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,10 +1,16 @@
 'use strict';
+//function collectSameElements(collectionA, collectionB) {
+//  var commonElements = [];
+//  for (var i = 0; i < collectionA.length; i++ ){
+//    if (collectionB.includes(collectionA[i])){
+//      commonElements.push(collectionA[i]);
+//    }
+//  }
+//  return commonElements;
+//}
+
+
 function collectSameElements(collectionA, collectionB) {
-  var commonElements = [];
-  for (var i = 0; i < collectionA.length; i++ ){
-    if (collectionB.includes(collectionA[i])){
-      commonElements.push(collectionA[i]);
-    }
-  }
-  return commonElements;
+
+  return collectionA.filter( a => collectionB.includes(a))
 }
