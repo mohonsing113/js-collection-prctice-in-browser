9c06da8030c13f5837633ed5fc2a76f953a0405c
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-21 11:57:08 +0800

    finished lambda to practice 1-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,13 +1,22 @@
 'use strict';
 
+//function collectSameElements(collectionA, objectB) {
+//
+//  var collection = objectB["value"];
+//  var commonElements = [];
+//  for (var i = 0; i < collectionA.length; i++ ){
+//    if (collection.includes(collectionA[i].key)){
+//      commonElements.push(collectionA[i].key);
+//    }
+//  }
+//  return commonElements;
+//}
+
+
+
 function collectSameElements(collectionA, objectB) {
 
-  var collection = objectB["value"];
-  var commonElements = [];
-  for (var i = 0; i < collectionA.length; i++ ){
-    if (collection.includes(collectionA[i].key)){
-      commonElements.push(collectionA[i].key);
-    }
-  }
-  return commonElements;
+  return collectionA
+    .filter( a => objectB.value.includes(a.key))
+    .map(e => e.key)
 }
